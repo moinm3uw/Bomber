74f4e8ebeb6649a947b3c0ee2721273d9b61b8d7
JanSeliv <janseliw@gmail.com>
2025-09-06 02:18:32 -0400

    [Minor] Removed unused level streaming property

--- Source/Bomber/Public/DataAssets/GeneratedMapDataAsset.h
+++ Source/Bomber/Public/DataAssets/GeneratedMapDataAsset.h
@@ -9,33 +9,6 @@
 #include "GeneratedMapDataAsset.generated.h"
 
 /**
- * Unique data about one separated level.
- */
-USTRUCT(BlueprintType)
-struct BOMBER_API FLevelStreamRow
-{
-	GENERATED_BODY()
-
-	/** The empty mesh row. */
-	static const FLevelStreamRow Empty;
-
-	/** Default constructor */
-	FLevelStreamRow() = default;
-
-	/** The level asset */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Level")
-	TSoftObjectPtr<class UWorld> Level = nullptr;
-
-	/** The associated type of a level. */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Level")
-	ELevelType LevelType = ELT::None;
-
-	/** The name of a level on UI. */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Level")
-	FText LevelName = TEXT_NONE;
-};
-
-/**
  * Contains all data that describe all levels.
  */
 UCLASS(Blueprintable, BlueprintType)
@@ -52,10 +25,6 @@ public:
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++")
 	const FORCEINLINE FGeneratedMapSettings& GetGenerationSettings() const { return GenerationSettingsInternal; }
 
-	/** Returns level rows. */
-	UFUNCTION(BlueprintPure, Category = "C++")
-	void GetLevelStreamRows(TArray<FLevelStreamRow>& OutRows) const { OutRows = LevelsInternal; }
-
 	/** Get UGeneratedMapDataAsset::CollisionsAssetInternal. */
 	UFUNCTION(BlueprintPure, Category = "C++")
 	FORCEINLINE TSubclassOf<class AActor> GetCollisionsAssetClass() const { return CollisionsAssetInternal; }
@@ -65,10 +34,6 @@ protected:
 	 * These settings might be overridden by Class Defaults of the Generated Map itself. */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BlueprintProtected, DisplayName = "Generation Settings", ShowOnlyInnerProperties))
 	FGeneratedMapSettings GenerationSettingsInternal;
-
-	/** Contains all used levels. */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BlueprintProtected, DisplayName = "Levels", TitleProperty = "LevelType", ShowOnlyInnerProperties))
-	TArray<FLevelStreamRow> LevelsInternal;
 
 	/** Asset that contains scalable collision. */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BlueprintProtected, DisplayName = "Collisions Asset", ShowOnlyInnerProperties))
