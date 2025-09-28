1165fae6c7556fafc2c8dae6c3521f9292a57797
JanSeliv <janseliw@gmail.com>
2025-09-27 23:53:50 -0400

    [Minor] Removed unused powerups data: skate property was already moved to the mover mode, curve table property to Gameplay Effects as configs

--- Source/Bomber/Public/DataAssets/ItemDataAsset.h
+++ Source/Bomber/Public/DataAssets/ItemDataAsset.h
@@ -42,28 +42,7 @@ public:
 	/** Returns the item data asset. */
 	static const UItemDataAsset& Get();
 
-	/** Returns speed value that is added to the player speed on taking a skate item.
-	 * @see UItemDataAsset::SkateStrengthInternal */
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++")
-	FORCEINLINE float GetSkateAdditiveStrength() const { return SkateAdditiveStrengthInternal; }
-
 	/** Return row by specified item type. */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++")
 	const UItemRow* GetRowByItemType(FBmrPowerupTag ItemType, ELevelType LevelType) const;
-
-	/** Returns the default powerups curve table that is used to set the default powerups levels for each character.
-	 * @see UItemDataAsset::DefaultPowerupsCurveTableInternal */
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "C++")
-	const FORCEINLINE class UCurveTable* GetDefaultPowerupsCurveTable() const { return DefaultPowerupsCurveTableInternal; }
-
-protected:
-	/** The speed additive value when player takes the skate item. */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BlueprintProtected, DisplayName = "Skate Additive Strength", ShowOnlyInnerProperties))
-	float SkateAdditiveStrengthInternal = 500.f;
-
-	/** The default powerups curve table that is used to set the default powerups levels for each character.
-	 * Columns (Item Type): Skate, Bomb, Fire
-	 * Rows (Player Tag): 'Player.Bastet' etc */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BlueprintProtected, DisplayName = "Default Powerups Curve Table", ShowOnlyInnerProperties))
-	TObjectPtr<const UCurveTable> DefaultPowerupsCurveTableInternal = nullptr;
 };
